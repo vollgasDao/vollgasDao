1191fad9f6af8c00aa2bba43b12d94ee629e7d24
gitpusha <gitpusha@gmail.com>
2019-08-27 09:45:14 +0200

    Revert "Delete truffle-config.js"
    This reverts commit 5d601e973675d88755a1927b49fc3e33d7c34fe9.

---
+++ truffle-config.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+const MNEMONIC = "ask settle radar stove person document recipe alien expose similar frost gasp";
+const RPC_URL = "https://rinkeby.infura.io/v3/b23fadbe7aa24194bc8bc9d86498eb38";
+
+
+module.exports = {
+  networks: {
+    cldev: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*'
+    },
+    live: {
+      provider: () => {
+        return new HDWalletProvider(MNEMONIC, RPC_URL)
+      },
+      network_id: '*',
+      // Necessary due to https://github.com/trufflesuite/truffle/issues/1971
+      // Should be fixed in Truffle 5.0.17
+      skipDryRun: true
+    }
+  },
+  compilers: {
+    solc: {
+      version: '0.4.24'
+    }
+  }
+}
