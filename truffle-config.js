8fb2f87ea25d50c8bae3652e5e0d4b8b5109f89f
gitpusha <gitpusha@gmail.com>
2019-08-28 12:03:32 +0200

    truffle config edits

---
+++ truffle-config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+const MNEMONIC = process.env["MNEMONIC"];
+const RPC_URL = process.env.RPC_URL;
+console.log(MNEMONIC);
+console.log(RPC_URL);
+
+module.exports = {
+  networks: {
+    cldev: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*'
+    },
+    live: {
+      provider: () => {
+        return new HDWalletProvider(MNEMONIC, RPC_URL)
+      },
+      network_id: '*',
+      // Necessary due to https://github.com/trufflesuite/truffle/issues/1971
+      // Should be fixed in Truffle 5.0.17
+      skipDryRun: true
+    }
+  },
+  compilers: {
+    solc: {
+      version: '0.4.24'
+    }
+  }
+}
